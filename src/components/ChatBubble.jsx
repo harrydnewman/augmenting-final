f01c303d8799693ffb62138b85e48f5032e7a890
Harrison Newman <harrydnewman@gmail.com>
2024-11-12 22:02:34 +0100

    setting stuff up

---
+++ src/components/ChatBubble.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/ChatBubble.module.css'
+
+export default function ChatBubble(){
+    return (
+        <div>
+            <h1>Chat Bubble</h1>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-
+import ChatBubble from "../components/ChatBubble"
+import styles from '../styles/Home.module.css'
 
 export default function Home(){
     return (
         <div>
             <h1>Home</h1>
+            <ChatBubble/>
         </div>
     )
 }
