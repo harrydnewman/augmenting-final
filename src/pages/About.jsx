848663845dcc849c24e2664f270e479920915ced
Harrison Newman <harrydnewman@gmail.com>
2024-11-26 03:57:21 +0100

    basic about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,13 @@
-
+import styles from '../styles/About.module.css'
 
 export default function About() {
     return (
         <div>
-            <div>
+            <div className={styles.AboutHeader}>
                 <h1>Chat 💬</h1>
                 <h1>Technology</h1>
             </div>
-            <div>
+            <div className={styles.AboutInfo}>
                 <p>Chats are instant shorthand conversations. They are often back and forth</p>
 
                 <p>Throughout history, our modes of chatting have changed based on the tools we've used.</p>
