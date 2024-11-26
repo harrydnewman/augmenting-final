d36830188160624e98241bace84f1045c76c99e9
Harrison Newman <harrydnewman@gmail.com>
2024-11-26 03:53:42 +0100

    Adding about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Graph from './pages/Graph';
 import BotFillerPage from './pages/BotFillerPage';
-
+import About from './pages/About';
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About/>}/>
           <Route path='/graph' element={<Graph/>}/>
           <Route path='/test' element={<BotFillerPage/>}/>
         </Routes>
---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+
+
+export default function About() {
+    return (
+        <div>
+            <div>
+                <h1>Chat 💬</h1>
+                <h1>Technology</h1>
+            </div>
+            <div>
+                <p>Chats are instant shorthand conversations. They are often back and forth</p>
+
+                <p>Throughout history, our modes of chatting have changed based on the tools we've used.</p>
+            </div>
+        </div>
+    )
+
+}
