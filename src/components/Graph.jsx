3f35386d3411866d3d5cbf0ce3fec7c8d81ae9b3
Harrison Newman <harrydnewman@gmail.com>
2024-12-08 22:38:42 +0100

    first iteration works, really ugly

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,11 +1,13 @@
 import styles from '../styles/Graph.module.css'
-
+import ScatterPlot from './LinePlot'
 export default function Graph({title, subtitle, blurb, data, xText, yText, zeroText}){
     return (
         <div className={styles.GraphDiv}>
             <h2><strong>{title}:</strong> {subtitle}</h2>
 
             <p>{blurb}</p>
+
+            <ScatterPlot data={data} xText={xText} yText={yText} zeroText={zeroText} />
 
 
         </div>
--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -2,8 +2,12 @@ import styles from '../styles/Compare.module.css'
 import CompareButton from '../components/CompareButton'
 import Graph from '../components/Graph'
 
-
 export default function Compare(){
+    const data = [
+        { x: 10, y: 20, label: "A", link: "/information", icon: "🗿" },
+        { x: 40, y: 90, label: "B", link: null, icon: "🎭" },
+        { x: 80, y: 50, label: "C", link: null, icon: "💀" },
+      ];
     return (
         <div className={styles.ComparePage}>
         <div className={styles.CompareTitleDiv}>
@@ -15,7 +19,7 @@ export default function Compare(){
             <CompareButton emoji={"🏓"} text={"two-way"}/>
             <CompareButton emoji={"🎭"} text={"personality"}/>
         </div>
-        <Graph title={"Speed"} subtitle={"the instantaneousness of our communication"} blurb={"Changes how much time it takes to reach us & how long we can think about the conversation"}/>
+        <Graph title={"Speed"} subtitle={"the instantaneousness of our communication"} blurb={"Changes how much time it takes to reach us & how long we can think about the conversation"} data={data} xText={"now"} yText={"instant"} zeroText={"snail mail"}/>
             {/* on click the return button needs to actually do something, but leave it for now */}
         </div>
     )
