e53ae4f4eeaf4209dc76c1374b9a002c196e985c
Harrison Newman <harrydnewman@gmail.com>
2024-12-09 19:04:12 +0100

    looks pretty, adding functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,17 @@ export default function Navbar({ currentPage }) {
     return (
         <div className={styles.Navbar}>
             <div className={styles.imessage}>
-                <p className={styles.fromThem}>❓ about</p>
+                <p className={styles.fromThem}>❓about</p>
+            </div>
+            <div className={styles.unselectedButton}>
+                <p>📈</p>
+            </div>
+            <div className={styles.unselectedButton}>
+                <p>🕵️‍♀️</p>
+            </div>
+            <div className={styles.unselectedButton}>
+                <p>✨</p>
             </div>
         </div>
-
     )
 }
