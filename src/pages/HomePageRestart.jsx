bd73aee1cc9cde81fb3ed741f3c17696ce2bc2fb
Harrison Newman <harrydnewman@gmail.com>
2024-12-09 21:01:36 +0100

    set up home

--- src/pages/HomePageRestart.jsx
+++ src/pages/HomePageRestart.jsx
@@ -5,6 +5,12 @@ export default function HomePageRestart(){
     return (
         <div>
             <h1>Home Page Restart</h1>
+
+            {/* create incoming chat bubble */}
+            {/* create outgoing chat bubble */}
+            {/* animate the css */}
+            {/* 3  dots chatbubble*/}
+            {/* create title and enter button*/}
         </div>
     )
 }
