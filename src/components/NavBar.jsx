f06d69d3a03f040dbe63c97349fe2abf330c0384
Harrison Newman <harrydnewman@gmail.com>
2024-12-09 18:32:13 +0100

    resettingNavbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,27 +3,9 @@ import styles from '../styles/NavbarStyles.module.css'
 export default function Navbar({ currentPage }) {
     return (
         <div className={styles.Navbar}>
-         <div className={styles.otherOptions}>
-                <p>❓</p>
-            </div>
-            <div className={styles.otherOptions}>
-                <p>📈</p>
-            </div>
-            <div className={styles.otherOptions}>
-                <p>🕵️‍♀️</p>
-            </div>
-            <div className={styles.otherOptions}>
-                <p>✨</p>
-            </div>
-
-            {/* <div className={styles.imessage}>
+            <div className={styles.imessage}>
                 <p className={styles.fromThem}>❓ about</p>
-                
-                
-            </div> */}
-
-
-
+            </div>
         </div>
 
     )
